67b8bfa11c288b0c73ddea745f8dd0a833507834
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2020-02-08 05:50:09 +0100

    Add state
    add onClick Handler for burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,43 @@
-import React, { Component } from 'react';
-import { ReactComponent as IconMenu } from '../assets/menu.svg';
-import './Header.css';
+import React, { Component } from "react";
+import { ReactComponent as IconMenu } from "../assets/menu.svg";
+import "./Header.css";
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+  }
+  changeStyle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
   render() {
     return (
       <nav>
-        <a className='logo' href='/'>
+        <a className="logo" href="/">
           Rozzi
         </a>
-        <div className='nav-links'>
-          <a className='nav-link' href='#projects'>
+        <div className="nav-links-horizontal">
+          <a className="nav-link" href="#projects">
             Projects
           </a>
-          <a className='nav-link' href='#contacts'>
+          <a className="nav-link" href="#contacts">
             Contacts
           </a>
         </div>
-        <IconMenu className='icon-menu' />
+        <div className={this.state.isOpen ? "nav-links-vertical" : "hidden"}>
+          <div className="nav-links">
+            <a className="nav-link" href="#projects">
+              Projects
+            </a>
+            <a className="nav-link" href="#contacts">
+              Contacts
+            </a>
+          </div>
+        </div>
+        <IconMenu className="icon-menu" onClick={this.changeStyle} />
       </nav>
     );
   }
