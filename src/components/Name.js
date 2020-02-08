433eb5940d096fefeee254dbe3cbe09f33cead31
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2020-02-08 06:02:59 +0100

    Update Name.js

--- src/components/Name.js
+++ src/components/Name.js
@@ -4,6 +4,7 @@ export default class Name extends Component {
   render() {
     return (
       <div>
+        <h1>Hahahahaahah</h1>
         <h1>Personal Information</h1>
         <h3>Name : Fahrur Rozzi</h3>
         <h3>Address : Mauritsstraat 288, 5615 RZ Eindhoven, Netherlands</h3>
