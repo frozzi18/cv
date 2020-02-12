ab229850b01cc6141c02936970daeacaf3228e83
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2020-02-12 11:37:26 +0100

    chore(tailwind) : adding tailwind configuration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-xl">
+      <h1 className="text-4xl">Hahahaa</h1>
       <Header />
     </div>
   );
