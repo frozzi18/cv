d5d2995acaac96637731146e1f7b0d8a75f8290f
Izzuddin Natsir <natsirizzuddin@gmail.com>
2020-02-16 11:41:40 +0700

    comp(hero section): Add animations, adjust responsive styles from 1240px screen wide to more than 2560px screen wide

--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/Header";
+import React from 'react';
+import { motion } from 'framer-motion';
 
 function App() {
   return (
-    <div className="App text-xl">
-      <Header />
-    </div>
+    <main>
+      <section className='relative flex items-center w-screen h-screen px-20 bg-yellow-100 4xl:justify-center 4xl:px-0'>
+        <motion.div
+          initial={{ y: '-50vh' }}
+          animate={{ y: 0 }}
+          transition={{ delay: 1, duration: 1 }}
+          className='absolute top-0 left-0 right-0 h-s-50 bg-white z-0'
+        />
+        <div className='relative flex flex-col justify-center w-full h-s-84 4xl:w-1760'>
+          <div className='flex justify-between items-end'>
+            <motion.h1
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.8, duration: 1.5 }}
+              className='giant-heading text-gray-900'
+            >
+              Hello
+              <span className='isolated-comma'>,</span>
+            </motion.h1>
+            <div className='mr-40 w-64 pb-5 xl:mr-64 2xl:pb-10'>
+              <p className='max-w-2xs leading-relaxed'>
+                <span className='font-semibold text-blue-900'>
+                  A full-stack web developer.
+                </span>{' '}
+                Coming from an engineering background, I make things work
+                correctly and beautifully.
+              </p>
+            </div>
+          </div>
+          <div>
+            <motion.h1
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1.2, duration: 1.5 }}
+              className='giant-heading text-gray-900'
+            >
+              I am Fahrur
+            </motion.h1>
+          </div>
+          <div className='flex justify-between items-baseline'>
+            <motion.h1
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1.6, duration: 1.5 }}
+              className='giant-heading text-gray-900'
+            >
+              Rozzi.
+            </motion.h1>
+            <div className='mr-24 w-80 xl:mr-48'>
+              <nav className='grid grid-flow-col col-gap-10 font-semibold uppercase text-blue-900'>
+                <a href='/' className='hover:text-orange-700 hover'>
+                  Explore projects
+                </a>
+                <a href='/' className='hover:text-orange-700 hover'>
+                  Know me more
+                </a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,39 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Cooper Hewitt']
+      },
+      maxWidth: {
+        '2xs': '15rem'
+      },
+      height: {
+        's-50': '50vh',
+        's-84': '84vh'
+      },
+      width: {
+        '72': '18rem',
+        '80': '20rem',
+        '1760': '1760px'
+      },
+      screens: {
+        '2xl': '1440px',
+        '3xl': '1600px',
+        '4xl': '1920px',
+        '5xl': '2560px'
+      },
+      fontSize: {
+        jumbo: '20rem'
+      },
+      lineHeight: {
+        overlap: '0.95'
+      },
+      padding: {
+        '7': '1.75rem',
+        '9': '2.25rem'
+      }
+    }
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
